188c0b680cf18d9fb1a60fd96367e7ead70ed867
masteroogway8374 <150107264+masteroogway8374@users.noreply.github.com>
2026-05-11 21:31:39 -0400

    Update sniffer.py

--- src/sniffer.py
+++ src/sniffer.py
@@ -1,16 +1,19 @@
 from scapy.all import rdpcap, IP, TCP, UDP
 from datetime import datetime
 
+# class used to load and process packet capture files
 class PacketSniffer:
     def __init__(self, pcap_file=None):
         self.pcap_file = pcap_file
         self.packets = []
 
+    # loads packets from the PCAP file
     def load_pcap(self):
         print(f"[*] Loading PCAP file: {self.pcap_file}")
 
         raw_packets = rdpcap(self.pcap_file)
 
+        # loops through all captured packets
         for packet in raw_packets:
             pkt_data = {
                 "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
@@ -25,16 +28,19 @@ class PacketSniffer:
                 "flags": None
             }
 
+            # checks if the packet contains IP data
             if IP in packet:
                 pkt_data["src_ip"] = packet[IP].src
                 pkt_data["dst_ip"] = packet[IP].dst
 
+                # checks if the packet is using TCP
                 if TCP in packet:
                     pkt_data["protocol"] = "TCP"
                     pkt_data["src_port"] = packet[TCP].sport
                     pkt_data["dst_port"] = packet[TCP].dport
                     pkt_data["flags"] = str(packet[TCP].flags)
 
+                # checks if the packet is using UDP
                 elif UDP in packet:
                     pkt_data["protocol"] = "UDP"
                     pkt_data["src_port"] = packet[UDP].sport
